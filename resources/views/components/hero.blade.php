24bd5beee09ed88bbf64468565b69f0ef6ba03a6
AsyaaEv <asyaaeval@gmail.com>
2023-11-13 09:32:41 +0700

    Hero section view

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,10 +1,26 @@
-<div class="container">
-    <div class="title">
+<section>
+    <div class="w-full h-screen">
+        <div class="w-full h-auto flex justify-center items-center mt-[5rem]">
+            <div class="w-[10rem] h-[2.5rem] bg-white shadow-lg rounded-[27px] flex justify-center items-center">
+                <div class="font-bold">Halo Semua!👋</div>
+            </div>
+        </div>
+        <div class="w-full h-auto flex justify-center items-center flex-col mt-4">
+            <div class="font-bold text-text text-5xl">Dapatkan website profesional</div>
+            <div class="font-bold text-text text-5xl">untuk bisnis anda</div>
+        </div>
+        <div class="w-full h-auto px-[20rem] border-2 mt-2">
+            <div class="text-text border-2 opacity-[70%] text-center text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, ipsam sapiente esse, adipisci, recusandae totam dolores at obcaecati ad dolor excepturi voluptatum officia quas nesciunt!</div>
+        </div>
+        <div class="w-full h-auto flex justify-center items-center mt-4">
+            <div class="w-[13rem] h-[3rem] bg-dasar rounded-[27px] shadow-lg flex justify-center items-center">
+                <div class="text-white text-sm font-bold">Dapatkan Sekarang</div>
+            </div>
+        </div>
+        <div class="w-full h-auto mt-4 flex justify-center items-center flex-col">
+            <div class="text-sm text-text font-bold text-center">Instansi yang sudah percaya kami</div>
+            <div class="w-full h-auto"></div>
+            <div class="text-[2rem]">😱❤️😊😂🤣</div>
+        </div>
     </div>
-    <div class="subtitle">
-
-    </div>
-    <div class="btn">
-
-    </div>
-</div>
+</section>
