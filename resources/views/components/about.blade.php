5ef8927121d881e7beab04e904fc6dc0867ef621
AsyaaEv <asyaaeval@gmail.com>
2023-11-13 10:27:59 +0700

    About fix

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,9 +1,14 @@
 <section>
     <div class="w-full h-screen mt-[8rem]">
         <div class="w-full h-auto flex justify-center items-center">
-            <div class="w-[80%] h-[468px] bg-gradient-to-tr from-[#99b0ff]/50 rounded-[10px] shadow-lg flex">
-                <div class="w-[50%] h-auto border-2"></div>
-                <div class="w-[50%] h-auto border-2"></div>
+            <div class="w-[80%] h-[468px] bg-[#EFF2FE] rounded-[10px] shadow-lg flex border-t-2">
+                <div class="w-[50%] h-auto flex justify-center items-center flex-col">
+                    <img src="{{ asset('img/foto.svg')}}" alt="">
+                </div>
+                <div class="w-[50%] h-auto  flex flex-col justify-center items-center gap-[10px]">
+                    <div class=" font-bold text-5xl mr-[5rem]">About BogengTeam</div>
+                    <div class=" font-semibold text-text opacity-80">Perusahaan BogengTeam adalah ahli dalam menyediakan jasa pembuatan website yang inovatif dan berkualitas tinggi. Dengan tim profesional yang berkomitmen, kami memahami bahwa setiap klien memiliki kebutuhan unik, dan itulah mengapa kami menawarkan solusi yang disesuaikan</div>
+                </div>
             </div>
         </div>
     </div>
