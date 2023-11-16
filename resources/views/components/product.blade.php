ec477352cf39c80949a6c56577267b7c2b2d8a92
diandra6695 <andracom108@gmail.com>
2023-11-16 11:47:31 +0700

    bug fix

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -4,11 +4,11 @@
             <div class="text-center text-text font-bold text-3xl">Our Product</div>
             <div class="text-center text-text opacity-60 font-bold text-lg">Pilihlah sesuai kebutuhan Anda</div>
         </div>
-        <div class="container w-full h-auto  mt-[3rem] flex gap-10 overflow-x-scroll">
-            <div class="w-[23rem] h-[26rem] rounded-[24px] shadow-sm bg-card flex justify-center items-center flex-col">
+        <div class="container w-full h-auto  mt-[3rem] flex gap-10 overflow-x-scroll scrollbar-hide">
+            <div class="rounded-[24px] p-4 shadow-sm bg-card">
                 <img src="{{ asset('img/card.png') }}" alt=""
                     class="object-fit overflow-hidden w-[20rem] h-[12rem]">
-                <div class="w-full h-auto  px-4 mt-4">
+                <div class="w-full h-auto mt-4">
                     <div class="text-text font-bold text-xl">Personal Company Website</div>
                     <div class="text-text font-bold text-md opacity-60">Lorem ipsum dolor sit amet, consectetur
                         adipiscing elit...</div>
@@ -16,16 +16,16 @@
                 <div class="w-full h-auto  flex justify-center items-center mt-4">
                     <a href="#" class="decoration-transparent">
                         <div
-                            class="w-[20rem] h-[4rem] rounded-[27px] bg-white flex justify-center items-center hover:cursor-pointer hover:scale-[1.05] transition-all">
+                            class="w-[20rem] h-[3.5rem] rounded-[27px] bg-white flex justify-center items-center hover:cursor-pointer hover:scale-[1.05] transition-all">
                             <div class=" text-lg font-bold text-dasar">Dapatkan Sekarang</div>
                         </div>
                     </a>
                 </div>
             </div>
-            <div class="w-[23rem] h-[26rem] rounded-[24px] shadow-sm bg-card flex justify-center items-center flex-col">
+            <div class="rounded-[24px] p-4 shadow-sm bg-card">
                 <img src="{{ asset('img/card.png') }}" alt=""
                     class="object-fit overflow-hidden w-[20rem] h-[12rem]">
-                <div class="w-full h-auto  px-4 mt-4">
+                <div class="w-full h-auto mt-4">
                     <div class="text-text font-bold text-xl">Personal Company Website</div>
                     <div class="text-text font-bold text-md opacity-60">Lorem ipsum dolor sit amet, consectetur
                         adipiscing elit...</div>
@@ -33,16 +33,16 @@
                 <div class="w-full h-auto  flex justify-center items-center mt-4">
                     <a href="#" class="decoration-transparent">
                         <div
-                            class="w-[20rem] h-[4rem] rounded-[27px] bg-white flex justify-center items-center hover:cursor-pointer hover:scale-[1.05] transition-all">
+                            class="w-[20rem] h-[3.5rem] rounded-[27px] bg-white flex justify-center items-center hover:cursor-pointer hover:scale-[1.05] transition-all">
                             <div class=" text-lg font-bold text-dasar">Dapatkan Sekarang</div>
                         </div>
                     </a>
                 </div>
             </div>
-            <div class="w-[23rem] h-[26rem] rounded-[24px] shadow-sm bg-card flex justify-center items-center flex-col">
+            <div class="rounded-[24px] p-4 shadow-sm bg-card">
                 <img src="{{ asset('img/card.png') }}" alt=""
                     class="object-fit overflow-hidden w-[20rem] h-[12rem]">
-                <div class="w-full h-auto  px-4 mt-4">
+                <div class="w-full h-auto mt-4">
                     <div class="text-text font-bold text-xl">Personal Company Website</div>
                     <div class="text-text font-bold text-md opacity-60">Lorem ipsum dolor sit amet, consectetur
                         adipiscing elit...</div>
@@ -50,41 +50,7 @@
                 <div class="w-full h-auto  flex justify-center items-center mt-4">
                     <a href="#" class="decoration-transparent">
                         <div
-                            class="w-[20rem] h-[4rem] rounded-[27px] bg-white flex justify-center items-center hover:cursor-pointer hover:scale-[1.05] transition-all">
-                            <div class=" text-lg font-bold text-dasar">Dapatkan Sekarang</div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="w-[23rem] h-[26rem] rounded-[24px] shadow-sm bg-card flex justify-center items-center flex-col">
-                <img src="{{ asset('img/card.png') }}" alt=""
-                    class="object-fit overflow-hidden w-[20rem] h-[12rem]">
-                <div class="w-full h-auto  px-4 mt-4">
-                    <div class="text-text font-bold text-xl">Personal Company Website</div>
-                    <div class="text-text font-bold text-md opacity-60">Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit...</div>
-                </div>
-                <div class="w-full h-auto  flex justify-center items-center mt-4">
-                    <a href="#" class="decoration-transparent">
-                        <div
-                            class="w-[20rem] h-[4rem] rounded-[27px] bg-white flex justify-center items-center hover:cursor-pointer hover:scale-[1.05] transition-all">
-                            <div class=" text-lg font-bold text-dasar">Dapatkan Sekarang</div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="w-[23rem] h-[26rem] rounded-[24px] shadow-sm bg-card flex justify-center items-center flex-col">
-                <img src="{{ asset('img/card.png') }}" alt=""
-                    class="object-fit overflow-hidden w-[20rem] h-[12rem]">
-                <div class="w-full h-auto  px-4 mt-4">
-                    <div class="text-text font-bold text-xl">Personal Company Website</div>
-                    <div class="text-text font-bold text-md opacity-60">Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit...</div>
-                </div>
-                <div class="w-full h-auto  flex justify-center items-center mt-4">
-                    <a href="#" class="decoration-transparent">
-                        <div
-                            class="w-[20rem] h-[4rem] rounded-[27px] bg-white flex justify-center items-center hover:cursor-pointer hover:scale-[1.05] transition-all">
+                            class="w-[20rem] h-[3.5rem] rounded-[27px] bg-white flex justify-center items-center hover:cursor-pointer hover:scale-[1.05] transition-all">
                             <div class=" text-lg font-bold text-dasar">Dapatkan Sekarang</div>
                         </div>
                     </a>
